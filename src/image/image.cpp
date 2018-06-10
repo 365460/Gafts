f83c8556490e7237558364ddab8fc338c3977072
SheepRanger <milk500ted939@gmail.com>
2018-06-11 01:16:12 +0800

    Ending second

--- src/image/image.cpp
+++ src/image/image.cpp
@@ -7,10 +7,10 @@ void Image::load(const char *texture) {
     /* Initialize shader */
     float vertices[] = {
         // position           // color            // texture coordinate
-         1.0f,  1.0f, 0.0f,   1.0f, 1.0f, 1.0f,   1.0f, 1.0f, // top right
-         1.0f, -1.0f, 0.0f,   1.0f, 1.0f, 1.0f,   1.0f, 0.0f, // bottom right
-        -1.0f, -1.0f, 0.0f,   1.0f, 1.0f, 1.0f,   0.0f, 0.0f, // bottom left
-        -1.0f,  1.0f, 0.0f,   1.0f, 1.0f, 1.0f,   0.0f, 1.0f  // top left
+         1.0f,  1.0f, 0.0f,   1.0f, 1.0f, 1.0f,   1.0f, 0.0f, // top right
+         1.0f, -1.0f, 0.0f,   1.0f, 1.0f, 1.0f,   1.0f, 1.0f, // bottom right
+        -1.0f, -1.0f, 0.0f,   1.0f, 1.0f, 1.0f,   0.0f, 1.0f, // bottom left
+        -1.0f,  1.0f, 0.0f,   1.0f, 1.0f, 1.0f,   0.0f, 0.0f  // top left
     };
     unsigned int indices[] = {
         0, 1, 3,
